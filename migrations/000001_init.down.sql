dbc10969f54f97531e2e7385b5d049329b88d575
simonkefir <romanttall@gmail.com>
2026-06-27 09:28:34 +0500

    infra environment was succesfully setup

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE messenger.messages;
+DROP TABLE messenger.chats_paticipants;
+DROP TABLE messenger.chats;
+DROP TABLE messenger.users;
+DROP SCHEMA messenger;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA messenger;
+
+CREATE TABLE messenger.users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(40) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW ()
+);
+
+CREATE TABLE messenger.chats (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE messenger.chats_participants (
+    chat_id INTEGER REFERENCES messenger.chats(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES messenger.users(id) ON DELETE CASCADE,
+    PRIMARY KEY (chat_id, user_id)
+);
+
+CREATE TABLE messenger.messages (
+    id SERIAL PRIMARY KEY,
+    chat_id INTEGER REFERENCES messenger.chats(id) ON DELETE CASCADE,
+    sender_id INTEGER REFERENCES messenger.users(id) ON DELETE CASCADE,
+    version BIGINT NOT NULL DEFAULT 1,
+    message TEXT NOT NULL,
+    is_delivered BOOLEAN NOT NULL DEFAULT FALSE,
+    sent_at TIMESTAMP DEFAULT NOW()
+);
